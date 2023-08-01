c20d39db4a329adf44c33dd42e1fee8f914ea167
Shamikh Hameed <63804919+ShamikhHameed@users.noreply.github.com>
2023-08-01 16:33:51 +0530

    exercise 1.7 completed

--- part1/unicafe/App.js
+++ part1/unicafe/App.js
@@ -17,20 +17,52 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
 
   const setToGood = (newGood) => {
-    console.log("Good value now", good);
+    console.log("Good value now", newGood);
     setGood(newGood);
+    const newAll = all + 1;
+    setToAll(newAll);
+    setToPositive(newGood, newAll);
+    setToAverage(newGood, bad, newAll);
   };
 
   const setToNeutral = (newNeutral) => {
-    console.log("Neutral value now", neutral);
+    console.log("Neutral value now", newNeutral);
     setNeutral(newNeutral);
+    const newAll = all + 1;
+    setToAll(newAll);
+    setToPositive(good, newAll);
+    setToAverage(good, bad, newAll);
   };
 
   const setToBad = (newBad) => {
-    console.log("Bad value now", bad);
+    console.log("Bad value now", newBad);
     setBad(newBad);
+    const newAll = all + 1;
+    setToAll(newAll);
+    setToPositive(good, newAll);
+    setToAverage(good, newBad, newAll);
+  };
+
+  const setToAll = (newAll) => {
+    console.log("All value now", newAll);
+    setAll(newAll);
+  };
+
+  const setToAverage = (updatedGood, updatedBad, updatedAll) => {
+    const avgVal = (updatedGood - updatedBad) / updatedAll;
+    console.log("Average value now", avgVal);
+    setAverage(avgVal);
+  };
+
+  const setToPositive = (updatedGood, updatedAll) => {
+    const positiveVal = (updatedGood / updatedAll) * 100 + " %";
+    console.log("Positive value now", positiveVal);
+    setPositive(positiveVal);
   };
 
   return (
@@ -46,6 +78,9 @@ const App = () => {
       <Display feedbackType="good" value={good} />
       <Display feedbackType="neutral" value={neutral} />
       <Display feedbackType="bad" value={bad} />
+      <Display feedbackType="all" value={all} />
+      <Display feedbackType="average" value={average} />
+      <Display feedbackType="positive" value={positive} />
     </div>
   );
 };
