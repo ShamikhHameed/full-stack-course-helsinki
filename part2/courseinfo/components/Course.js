84f43ddd98fdd5607f87076c9bd9b7cf5a0c6a98
Shamikh Hameed <63804919+ShamikhHameed@users.noreply.github.com>
2023-08-07 20:24:47 +0530

    exercise 2.2 completed

--- part2/courseinfo/components/Course.js
+++ part2/courseinfo/components/Course.js
@@ -26,11 +26,20 @@ const Header = ({ name }) => {
     )
   }
 
+  const Total = ({ parts }) => {
+    const sum = parts.reduce((sum, part) => {
+        return sum + part.exercises
+    }, 0)
+
+    return <b>total of {sum} exercises</b>
+  }
+
   const Course = (props) => {
     return (
         <>
         <Header name={props.course.name}/>
         <Content parts={props.course.parts} />
+        <Total parts={props.course.parts} />
         </>
     )
   }
